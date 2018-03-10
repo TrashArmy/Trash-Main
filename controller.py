ea01a54172314d2dc26e5b41f42846cbbf106cc3
Stephen Brotman <Stephen@lawn-128-61-71-197.lawn.gatech.edu>
2018-03-10 16:17:42 -0500

    Adding MySQL insert statements to controller.py

--- controller.py
+++ controller.py
@@ -16,6 +16,9 @@ MYSQL_USER = 'remote'
 MYSQL_PASS = 'login'
 MYSQL_DB = 'SeniorDesign'
 MYSQL_PORT = 3306
+MYSQL_TABLE = 'TrashData'
+
+TRASH_ID = 0 # id of trashcan
 
 SERVO_1 = 0 # pin for servo 1
 SERVO_2 = 0 # pin for servo 2
@@ -27,33 +30,44 @@ ACTUATOR_REV = 0 # pin for rev control of linear actuator
 
 LIGHT = 0 # pin for light
 
-SONAR_1_ECHO = 20 # pin for sonar sensor 1 echo
-SONAR_1_TRIG = 21 # pin for sonar sensor 1 trigger
-SONAR_1_MIN = 6 # min distance for 100% [cm]
-SONAR_1_MAX = 25 # max distance for 0% [cm]
-SONAR_2_ECHO = 0 # pin for sonar sensor 2 echo
-SONAR_2_TRIG = 0 # pin for sonar sensor 2 trigger
-SONAR_3_ECHO = 0 # pin for sonar sensor 3 echo
-SONAR_3_TRIG = 0 # pin for sonar sensor 3 trigger
-SONAR_4_ECHO = 0 # pin for sonar sensor 4 echo
-SONAR_4_TRIG = 0 # pin for sonar sensor 4 trigger
+SONAR_0_ECHO = 20 # pin for sonar sensor 1 echo
+SONAR_0_TRIG = 21 # pin for sonar sensor 1 trigger
+SONAR_0_MIN = 6 # min distance for 100% [cm]
+SONAR_0_MAX = 25 # max distance for 0% [cm]
+SONAR_1_ECHO = 0 # pin for sonar sensor 2 echo
+SONAR_1_TRIG = 0 # pin for sonar sensor 2 trigger
+SONAR_2_ECHO = 0 # pin for sonar sensor 3 echo
+SONAR_2_TRIG = 0 # pin for sonar sensor 3 trigger
+SONAR_3_ECHO = 0 # pin for sonar sensor 4 echo
+SONAR_3_TRIG = 0 # pin for sonar sensor 4 trigger
 
 DOOR_SWITCH = 0 # pin for switch on door
 
 
 # Define functions
 
-def sonar_ping(s, cursor):
-    #print(s.read())
+def sonar_ping(id, s, conn, dmin, dmax):
+    p = 0
     r = s.read_cm_avg(100, 0.05)
-    if r >= SONAR_1_MAX:
-        print('0%')
-    elif r <= SONAR_1_MIN:
-        print('100%')
+    if r >= dmax:
+        p = 0
+    elif r <= dmin:
+        p = 100
     else:
-        v = ((SONAR_1_MAX-r)/(SONAR_1_MAX-SONAR_1_MIN))*100
-        print('{}%'.format(round(v)))
+        p = round(((dmax-r)/(dmax-dmin))*100)
+    
+    print('{}%'.format(p))
 
+    cursor = conn.cursor()
+    sql = "INSERT INTO {0} VALUES({1}, {2}, GETDATE(), {3});".format(
+          MYSQL_TABLE, TRASH_ID, id, p)
+    try:
+        cursor.execute(sql);
+        conn.commit()
+    except:
+        conn.rollback()
+
+# Main 
 
 if __name__ == '__main__':
     # Initialization
@@ -63,22 +77,28 @@ if __name__ == '__main__':
                            passwd = MYSQL_PASS,
                            db = MYSQL_DB, 
                            port = MYSQL_PORT)
-    cursor = conn.cursor()
 
+    if not pi:
+        printf('pi not initializing')
+        exit()
 
-    SONAR_1 = sonar.Sonar(pi, SONAR_1_TRIG, SONAR_1_ECHO)
+    if not conn:
+        printf('mySQL not connecting')
+        exit()
+
+    SONAR_0 = sonar.Sonar(pi, SONAR_0_TRIG, SONAR_0_ECHO)
 
     # Do stuff
     try:
         while(True):
-            sonar_ping(SONAR_1, cursor)
+            sonar_ping(0, SONAR_0, conn, SONAR_0_MIN, SONAR_0_MAX)
             #print('loop!')
             #time.sleep(1)
             time.sleep(0.03)
     except KeyboardInterrupt:
-        SONAR_1.cancel()
+        SONAR_0.cancel()
 
 
 
-
+    conn.close()
     # pi.stop()
