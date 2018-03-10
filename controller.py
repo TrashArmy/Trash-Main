f6fd864503f4770fb5fecf0f1197255ab37d9e63
Stephen Brotman <Stephen@lawn-128-61-71-197.lawn.gatech.edu>
2018-03-10 14:29:01 -0500

    Changed values to percentages in 'controller.py'

--- controller.py
+++ controller.py
@@ -22,6 +22,8 @@ LIGHT = 0 # pin for light
 
 SONAR_1_ECHO = 20 # pin for sonar sensor 1 echo
 SONAR_1_TRIG = 21 # pin for sonar sensor 1 trigger
+SONAR_1_MIN = 6 # min distance for 100% [cm]
+SONAR_1_MAX = 25 # max distance for 0% [cm]
 SONAR_2_ECHO = 0 # pin for sonar sensor 2 echo
 SONAR_2_TRIG = 0 # pin for sonar sensor 2 trigger
 SONAR_3_ECHO = 0 # pin for sonar sensor 3 echo
@@ -36,7 +38,14 @@ DOOR_SWITCH = 0 # pin for switch on door
 
 def sonar_ping(s):
     #print(s.read())
-    print(s.read_cm_avg(100, 0.05))
+    r = s.read_cm_avg(100, 0.05)
+    if r >= SONAR_1_MAX:
+        printf('0%')
+    elif r <= SONAR_1_MIN:
+        print('100%')
+    else:
+        v = ((SONAR_1_MAX-r)/(SONAR_1_MAX-SONAR_1_MIN))*100
+        print('%d\%'.format(round(v)))
 
 
 if __name__ == '__main__':
