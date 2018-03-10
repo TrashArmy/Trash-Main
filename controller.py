41ae178adb3af8db77b02e4fd75320602851f37e
Stephen Brotman <Stephen@lawn-128-61-71-197.lawn.gatech.edu>
2018-03-10 12:15:19 -0500

    Updated do stuff part of controller

--- controller.py
+++ controller.py
@@ -2,7 +2,7 @@
 
 Controls everything from here
 """
-
+import time
 import motor # motor controller
 import servo_HS805BB # servo controller
 import light # light controller
@@ -19,8 +19,8 @@ ACTUATOR_REV = 0 # pin for rev control of linear actuator
 
 LIGHT = 0 # pin for light
 
-SONAR_1_ECHO = 0 # pin for sonar sensor 1 echo
-SONAR_1_TRIG = 0 # pin for sonar sensor 1 trigger
+SONAR_1_ECHO = 20 # pin for sonar sensor 1 echo
+SONAR_1_TRIG = 21 # pin for sonar sensor 1 trigger
 SONAR_2_ECHO = 0 # pin for sonar sensor 2 echo
 SONAR_2_TRIG = 0 # pin for sonar sensor 2 trigger
 SONAR_3_ECHO = 0 # pin for sonar sensor 3 echo
@@ -31,13 +31,27 @@ SONAR_4_TRIG = 0 # pin for sonar sensor 4 trigger
 DOOR_SWITCH = 0 # pin for switch on door
 
 
+# Define functions
+
+def sonar_ping(s):
+   print(s.read_cm_avg(100))
 
 
 if __name__ == '__main__':
     # Initialization
     pi = pigpio.pi()
 
-    # do stuff
+    SONAR_1 = Sonar(pi, 20, 21)
+
+    # Do stuff
+    try:
+        while(True):
+            sonar_ping(SONAR_1)
+            time.sleep(1)
+    except KeyboardInterrupt:
+        SONAR_1.cancel()
+
+
 
 
     # pi.stop()
