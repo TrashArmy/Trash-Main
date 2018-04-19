530380e1ac6d383b031f82b9c0fb30406a185bf1
mr-sagarmandal <sagar187@hotmail.com>
2018-04-18 21:16:57 -0400

    Adding Classification Components

--- CaptureImage.py
+++ CaptureImage.py
@@ -9,8 +9,8 @@ def capture(data_path, image_name):
         camera.resolution = (1024, 1024)
         file_name = data_path + "/" + image_name + ".jpg"
         camera.capture(file_name, resize=(480, 480))
-    
     return file_name
+
 
 if __name__ == '__main__':
     print("Import the file and call capture with path to image and image name")
--- controller.py
+++ controller.py
@@ -4,19 +4,42 @@ Controls everything from here
 """
 import time
 import pigpio
-# import picamera
+import os
+
+# Import IOT Components
 import MySQLdb
 # if MySQLdb not found, do:
 # sudo apt-get install mysql-server
 # sudo apt-get install mysql-client
 # sudo apt-get install python-mysqldb
+
+# Import mechanical contoller components
 import motor  # motor controller
 import servo_HS805BB  # servo controller
 import light  # light controller
 import sonar  # ultrasonic sensor controller
 import switch  # switch controller
-import os
-# import shutil
+
+# Importing components necessary for Computer Vision
+import numpy as np
+import sys
+import tensorflow as tf
+from collections import defaultdict
+from io import StringIO
+from matplotlib import pyplot as plt
+from PIL import Image
+
+sys.path.append('./object_detection')
+from object_detection.utils import ops as utils_ops
+
+if tf.__version__ < '1.4.0':
+  raise ImportError('Please upgrade your tensorflow installation to v1.4.* or later!')
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as vis_util
+
+
+
+#Importing Our Custom Packages
 import CaptureImage
 
 # Configuration
@@ -76,7 +99,15 @@ SONAR_3_MAX = 95.8  # max distance for 0% [cm]
 
 DOOR_SWITCH_PIN = 4  # pin for switch on door
 
-CONST_DATADIR = "./images/"
+
+IMAGE_DIR = "./images/"
+
+MODEL_FOLDER_NAME = 'classification_model'
+# Path to frozen detection graph. This is the actual model that is used for the object detection.
+PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
+# List of the strings that is used to add correct label for each box.
+PATH_TO_LABELS = os.path.join('classification_model', 'label_map.pbtxt')
+NUM_CLASSES = 4 # Number of classes classifier has
 
 # Define functions
 
@@ -123,7 +154,7 @@ def getDataDir(name):
     '''
     Creates a folder with any name in the directory
     '''
-    name = CONST_DATADIR + name
+    name = IMAGE_DIR + name
     name = validateDir(name)
     os.makedirs(name)
     return name
@@ -131,6 +162,19 @@ def getDataDir(name):
 
 def getDataFolderName():
     return time.strftime("%Y%m%d", time.gmtime())
+
+def loadImageIntoNumpyArray(image):
+  (im_width, im_height) = image.size
+  return np.array(image.getdata()).reshape(
+      (im_height, im_width, 3)).astype(np.uint8)
+
+def getLabelMapCategories():
+    label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+    categories = label_map_util.convert_label_map_to_categories(label_map,
+        max_num_classes=NUM_CLASSES, use_display_name=True)
+    category_index = label_map_util.create_category_index(categories)
+    return category_index
+
 
 # Main
 if __name__ == '__main__':
@@ -152,7 +196,7 @@ if __name__ == '__main__':
 
     if DOOR_SWITCH_EN:
         DOOR_SWITCH = switch.Switch(pi, DOOR_SWITCH_PIN)
-        
+
     if LIGHT_EN:
         LIGHT = light.Light(LIGHT_PIN)
         LIGHT.off()
@@ -165,7 +209,7 @@ if __name__ == '__main__':
         SERVO_0.degree(SERVO_0_CLOSE)
         SERVO_1.degree(SERVO_1_CLOSE)
         SERVO_2.degree(SERVO_2_CLOSE)
-        
+
         SERVO_0.off()
         SERVO_1.off()
         SERVO_2.off()
@@ -184,6 +228,17 @@ if __name__ == '__main__':
     data_path = getDataDir(getDataFolderName())
     image_count = 0
     LIGHT = light.Light(LIGHT_PIN)
+
+    # Loading Frozen Tensorflow model into memory
+    detection_graph = tf.Graph()
+    with detection_graph.as_default():
+      od_graph_def = tf.GraphDef()
+      with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+        serialized_graph = fid.read()
+        od_graph_def.ParseFromString(serialized_graph)
+        tf.import_graph_def(od_graph_def, name='')
+
+
 
     # Do stuff
     try:
@@ -208,14 +263,14 @@ if __name__ == '__main__':
             if LIGHT_EN:
                 LIGHT.on()
                 time.sleep(0.2)
-            
+
             if CAMERA_EN:
                 image_file = CaptureImage.capture(data_path, str(image_count))
                 image_count += 1
-                trash_id = 1
-            else:
-                trash_id = 3
-                
+
+
+
+
             if LIGHT_EN:
                 LIGHT.off()
 
